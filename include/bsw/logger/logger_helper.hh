ec966186186d7937b2d6a3606d6e5414da185e2a
igor <igor.gutnik@gmail.com>
2024-01-31 11:58:13 +0200

    fixes

--- include/bsw/logger/logger_helper.hh
+++ include/bsw/logger/logger_helper.hh
@@ -8,22 +8,21 @@
 
 #include "bsw/export.h"
 
-namespace bsw::logger
-{
-    void BSW_EXPORT disable();
-    void BSW_EXPORT post_record(const std::shared_ptr<record>& record);
+namespace bsw::logger {
+	void BSW_EXPORT disable ();
+	void BSW_EXPORT post_record (const std::shared_ptr<record>& record);
 
 } // ns logger
 
 
-#define EV_LOGGER_TRACE(SEVERITY, MODULE, FUNCTION, FILE, LINE, ...)	                    \
+#define EV_LOGGER_TRACE(SEVERITY, MODULE, FUNCTION, FILE, LINE, ...)                        \
   do {                                                                                      \
     using namespace ::bsw::logger;                                                          \
-    locus locus_ (MODULE, FILE, FUNCTION, LINE);			                                \
-    auto rec = std::make_shared<record> (SEVERITY, locus_);									\
-    priv::multi_setter_c ms (*rec.get ());					                                \
-    ms.set (__VA_ARGS__) ;						                                            \
-    post_record (rec);							                                            \
+    locus locus_ (MODULE, FILE, FUNCTION, LINE);                                            \
+    auto rec = std::make_shared<record> (SEVERITY, locus_);                                 \
+    priv::multi_setter_c ms (*rec.get ());                                                  \
+    ms.set (__VA_ARGS__) ;                                                                  \
+    post_record (rec);                                                                      \
   } while (0)
 
 #if !defined(TRACE_LEVEL)
@@ -34,39 +33,37 @@ namespace bsw::logger
 #   endif
 #endif // TRACE_LEVEL
 
-
-
 #if TRACE_LEVEL > 1
-#define EV_LOGGER_TRACE_EVLOG_FATAL(MODULE, FUNCTION, FILE, LINE, ...)	\
-  EV_LOGGER_TRACE (logger::eFATAL, MODULE, FUNCTION, FILE, LINE, ##__VA_ARGS__)
+#define EV_LOGGER_TRACE_EVLOG_FATAL(MODULE, FUNCTION, FILE, LINE, ...)    \
+  EV_LOGGER_TRACE (::bsw::logger::eFATAL, MODULE, FUNCTION, FILE, LINE, ##__VA_ARGS__)
 #else
 #define EV_LOGGER_TRACE_EVLOG_FATAL(MODULE, FUNCTION, FILE, LINE, ...)            ((void)0)
 #endif
 
 #if TRACE_LEVEL > 2
-#define EV_LOGGER_TRACE_EVLOG_ERROR(MODULE, FUNCTION, FILE, LINE, ...)	\
-  EV_LOGGER_TRACE (logger::eERROR, MODULE, FUNCTION, FILE, LINE,  ##__VA_ARGS__)
+#define EV_LOGGER_TRACE_EVLOG_ERROR(MODULE, FUNCTION, FILE, LINE, ...)    \
+  EV_LOGGER_TRACE (::bsw::logger::eERROR, MODULE, FUNCTION, FILE, LINE,  ##__VA_ARGS__)
 #else
 #define EV_LOGGER_TRACE_EVLOG_ERROR(MODULE, FUNCTION, FILE, LINE, ...)            ((void)0)
 #endif
 
 #if TRACE_LEVEL > 3
 #define EV_LOGGER_TRACE_EVLOG_WARNING(MODULE, FUNCTION, FILE, LINE, ...) \
-  EV_LOGGER_TRACE (logger::eWARNING, MODULE, FUNCTION, FILE, LINE, ##__VA_ARGS__)
+  EV_LOGGER_TRACE (::bsw::logger::eWARNING, MODULE, FUNCTION, FILE, LINE, ##__VA_ARGS__)
 #else
 #define EV_LOGGER_TRACE_EVLOG_WARNING(MODULE, FUNCTION, FILE, LINE, ...)            ((void)0)
 #endif
 
 #if TRACE_LEVEL > 4
-#define EV_LOGGER_TRACE_EVLOG_NOTICE(MODULE, FUNCTION, FILE, LINE, ...)	\
-  EV_LOGGER_TRACE (logger::eNOTICE, MODULE, FUNCTION, FILE, LINE,  ##__VA_ARGS__)
+#define EV_LOGGER_TRACE_EVLOG_NOTICE(MODULE, FUNCTION, FILE, LINE, ...)    \
+  EV_LOGGER_TRACE (::bsw::logger::eNOTICE, MODULE, FUNCTION, FILE, LINE,  ##__VA_ARGS__)
 #else
 #define EV_LOGGER_TRACE_EVLOG_NOTICE(MODULE, FUNCTION, FILE, LINE, ...)            ((void)0)
 #endif
 
 #if TRACE_LEVEL > 5
-#define EV_LOGGER_TRACE_EVLOG_INFO(MODULE, FUNCTION, FILE, LINE, ...)	\
-  EV_LOGGER_TRACE (logger::eINFO, MODULE, FUNCTION, FILE, LINE,  ##__VA_ARGS__)
+#define EV_LOGGER_TRACE_EVLOG_INFO(MODULE, FUNCTION, FILE, LINE, ...)    \
+  EV_LOGGER_TRACE (::bsw::logger::eINFO, MODULE, FUNCTION, FILE, LINE,  ##__VA_ARGS__)
 #else
 #define EV_LOGGER_TRACE_EVLOG_INFO(MODULE, FUNCTION, FILE, LINE, ...)                 ((void)0)
 #endif
